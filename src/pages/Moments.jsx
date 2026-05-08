22b8f53ee962d583c9eb012bf3567001bac2c3b5
Cloud Talk <cloudtalk89@gmail.com>
2026-05-08 19:23:20 +0630

    imagcard fix

--- src/pages/Moments.jsx
+++ src/pages/Moments.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import ImageCard from '../components/ImageCard';
+import image from '../assets/image.png'
 
 const Moments = () => {
   return (
@@ -8,10 +9,10 @@ const Moments = () => {
       <h2 className='text-lg md:text-3xl text-white font-bold flex items-center justify-between p-4 z-10'><Link to="/surprise"  className='p-2 hover:scale-110 border rounded-xl text-sm px-4 cursor-pointer bg-white text-violet-500 mr-6 transition-all duration-150'>Back</Link> <span>🎉 Happy Birthday My Baby 🎉</span></h2>
       
       <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 overflow-auto gap-4'>
-        <ImageCard src="../../public/image.png" text = "This is test"/>
-        <ImageCard src="../../public/image.png" text = "This is test"/>
-        <ImageCard src="../../public/image.png" text = "This is test"/>
-        <ImageCard src="../../public/image.png" text = "This is test"/>
+        <ImageCard src={image} text="This is test" />
+        <ImageCard src={image} text="This is test" />
+        <ImageCard src={image} text="This is test" />
+        <ImageCard src={image} text="This is test" />
       </section>
 
     </section>
